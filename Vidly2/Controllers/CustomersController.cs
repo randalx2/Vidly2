ffc6855e1374f280e70559a54910f9cfd931d111
Praneel Misthry <pmisthry@gmail.com>
2017-09-14 00:03:03 +0200

    Add customers controller

---
+++ Vidly2/Controllers/CustomersController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly2.Models;
+
+namespace Vidly2.Controllers
+{
+    public class CustomersController : Controller
+    {
+        //Create the db context
+        private ApplicationDbContext _context;
+
+        //Initialize the db context in the constructor
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //Properly dispose of the db context
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = _context.Customers.ToList();
+
+            return View(customers);
+        }
+
+    }
+}
