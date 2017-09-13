dd5eb4464a599f32a6d5ab61ff3052e146bfc120
Praneel Misthry <pmisthry@gmail.com>
2017-09-13 23:29:02 +0200

    Added membership types

--- Vidly2/Models/Customer.cs
+++ Vidly2/Models/Customer.cs
@@ -8,6 +8,15 @@ namespace Vidly2.Models
     public class Customer
     {
         public int Id { get; set; }
-        public string Name { get; set; }    
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }  
+
+        //Navigation property
+        public MembershipType MembershipType { get; set; }
+
+        //Foreign Key
+        //EF recognizes this convention and treats this property as a foreign key
+        public byte MembershipTypeId { get; set; }  
+
     }
 }
